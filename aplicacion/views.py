e38fa88be292fcd279d101276c4a618b3ad9aa65
Guille <g.bric90@gmail.com>
2016-06-30 01:12:14 -0400

    Cambie el HTML para la página.

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from aplicacion.models import Alumnos
+from django.shortcuts import render_to_response
 
 # Create your views here.
+
+def mostrar(request):
+ 	return render_to_response('mostrar.html',{})
--- prueba/settings.py
+++ prueba/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
 }
 
 
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
