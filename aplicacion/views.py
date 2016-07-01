9d11922b149aa4b21db6e5c32e31d945d24cae13
Guille <g.bric90@gmail.com>
2016-07-01 15:09:13 -0400

    Pruebas con d3.js

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render_to_response
 
 def mostrar(request):
  	return render_to_response('mostrar.html',{})
+
+def ej1(request):
+ 	return render_to_response('ej1.html',{})
+
+def ej2(request):
+ 	return render_to_response('ej2.html',{})
+
+def ej3(request):
+ 	return render_to_response('ej3.html',{})	
+
+def ej4(request):
+ 	return render_to_response('ej4.html',{})	 	
+
+def barra(request):
+ 	return render_to_response('barra6.html',{})	
--- prueba/urls.py
+++ prueba/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^$','aplicacion.views.mostrar'),
+	url(r'^ej1/','aplicacion.views.ej1'),
+	url(r'^ej2/','aplicacion.views.ej2'),
+	url(r'^ej3/','aplicacion.views.ej3'),
+	url(r'^ej4/','aplicacion.views.ej4'),
+	url(r'^barra/','aplicacion.views.barra'),
 ]
